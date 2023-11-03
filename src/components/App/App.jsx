cd4af9cc9b05c306b47db1d25914c94065b41f58
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-11-03 21:23:41 +0200

    DONE

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
 import { Container, Span } from './App.styled';
-
-function checkSavedContacts() {
-  const savedContacts = localStorage.getItem('contacts');
-  if (savedContacts == null) {
-    return [];
-  }
-  return JSON.parse(savedContacts);
-}
+import { getContacts } from 'redux/ContactsSlice';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(checkSavedContacts);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
+  const contacts = useSelector(getContacts);
   return (
     <Container>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       {contacts.length > 0 ? <Filter /> : <Span>Contact list is empty</Span>}
-
       <ContactList />
     </Container>
   );
